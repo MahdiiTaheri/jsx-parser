be2e44272438bfa27193c730a4dba32a7eccbf30
Mahdi Taheri <m.taheri123456789@gmail.com>
2025-04-21 13:07:43 +0330

    Chore: Add new components to registry

--- src/parser/registry.ts
+++ src/parser/registry.ts
@@ -7,13 +7,21 @@ export const componentRegistry: Record<string, ComponentConfig> = {
   Text: { component: "text", tagNameProp: "as", defaults: { as: "h1" } },
   Button: { component: "button" },
   Icon: { component: "icon" },
-  Divider: { component: "divider" },
   Form: { component: "form" },
   Input: { component: "input" },
   Alert: { component: "alert" },
   Accordion: { component: "accordion" },
   Switch: { component: "switch" },
   Separator: { component: "separator" },
+  AvatarUploader: { component: "avatarUploader" },
+  Chart: { component: "chart" },
+  Combobox: { component: "combobox" },
+  Command: { component: "command" },
+  DateShower: { component: "DateShower" },
+  FullscreenToggle: { component: "fullscreenToggle" },
+  MultiSelect: { component: "multiSelect" },
+  Radio: { component: "radio" },
+  Select: { component: "select" },
   // Additional components can be added here.
 };
 
